5cb5eed2c28ec36295221c793ad6ed132d9a2223
Heejeong Kim <sophisticated97@naver.com>
2020-03-20 09:49:00 +0900

    오픈 채팅방 주석 추가

--- Heejeong/오픈 채팅방.py
+++ Heejeong/오픈 채팅방.py
@@ -1,14 +1,20 @@
 def solution(record):
     answer = []
     uinfo = {}  # user info - uid, nicname
+
+    # record는 command uid nicname 순으로 들어옴.
+    # command의 종류: Enter, Leave, Change
     for r in record:
         command = r.split(" ")
+
+        # user의 닉네임은 Enter와 Change를 통해서 변함.
         if command[0] != "Leave": uinfo[command[1]] = command[2]
 
     for r in record:
         command = r.split(" ")
-        if command[0] == "Change":
-            continue
+
+        # 출력 메세지는 Enter와 Leave일 때만 기록됨.
+        if command[0] == "Change": continue
         elif command[0] == "Enter":
             answer.append("{}님이 들어왔습니다.".format(uinfo[command[1]]))
         else:
