a01eb2ee3f83c4385b5f3c6584556b1ef47398b4
ryanma <ryanma@coupang.com>
2020-03-23 16:02:21 +0900

    [RYAN] 기능개발

--- RYAN/기능개발.java
+++ RYAN/기능개발.java
@@ -1,7 +1,6 @@
-import java.util.List;
-import java.util.LinkedList;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import static java.util.Objects.nonNull;
 import static java.util.stream.Collectors.toCollection;
@@ -11,27 +10,26 @@ class Solution {
 	private static final int WORK_MAX_PROGRESS = 100;
 
 	public int[] solution(int[] progresses, int[] speeds) {
-        
+
 		List<Integer> completeCount = new ArrayList<>();
-        int partialCompleteCount = 0;
-        
+		int partialCompleteCount = 0;
+
 		Queue<Work> workQueue = generateWorkQueue(progresses, speeds);
 
 		while (!workQueue.isEmpty()) {
-			workQueue = workQueue.stream()
-				.peek(work -> work.progress = work.progress + work.speed)
-				.collect(toCollection(LinkedList::new));
-            
-            partialCompleteCount = 0;
-            
-            while (nonNull(workQueue.peek()) && workQueue.peek().progress > WORK_MAX_PROGRESS) {
-                workQueue.remove();
-                partialCompleteCount++;
-            }
-            
-            if (partialCompleteCount > 0) {
-                completeCount.add(partialCompleteCount);
-            }
+			workQueue = workQueue.stream().peek(work -> work.progress = work.progress + work.speed)
+					.collect(toCollection(LinkedList::new));
+
+			partialCompleteCount = 0;
+
+			while (nonNull(workQueue.peek()) && workQueue.peek().progress > WORK_MAX_PROGRESS) {
+				workQueue.remove();
+				partialCompleteCount++;
+			}
+
+			if (partialCompleteCount > 0) {
+				completeCount.add(partialCompleteCount);
+			}
 		}
 
 		return completeCount.stream().mapToInt(Integer::intValue).toArray();
@@ -49,7 +47,7 @@ class Solution {
 }
 
 class Work {
-    
+
 	int progress;
 	int speed;
 
