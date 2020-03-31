49096648603ae94671ce9b28afba42c92d8736ab
Heejeong Kim <sophisticated97@naver.com>
2020-03-31 10:24:34 +0900

    [Heejeong] 다음 큰 숫자

---
+++ Heejeong/다음 큰 숫자.py
@@ -0,0 +1,23 @@
+# 이진수(bin_num) 문자열을 입력받아 1의 개수를 반환하는 함수
+def countOne(bin_num):
+    return len(list(filter(lambda x: x == '1', bin_num)))
+
+def decimalToBinary(n):
+    return str(bin(n))[2:]
+
+def solution(n):
+    condition = countOne(decimalToBinary(n))
+    while True:
+        n += 1
+        if countOne(decimalToBinary(n)) == condition: break
+    return n
+
+if __name__ == "__main__":
+    test_case = [78, 15]
+    test_answer = [83, 23]
+
+    for i in range(len(test_case)):
+        answer = solution(test_case[i])
+        print("#{} {}".format(i+1, answer))
+        if answer == test_answer[i]: print("맞았습니다!")
+        else: print("틀렸습니다!")
