48ba4d97c8641d2259f3ff92a858266fc95e5c27
Heejeong Kim <sophisticated97@naver.com>
2020-03-31 11:34:57 +0900

    스킬트리 주석 추가

--- Heejeong/스킬트리.py
+++ Heejeong/스킬트리.py
@@ -1,6 +1,8 @@
-def isAvailable(skill, skills):
-    while skills:
-        element = skills.pop(0)
+# 가능한 스킬 트리인지 검사하는 함수
+def isAvailable(skill, skill_tree):
+    # 스킬트리에 값이 없을 때까지 반복
+    while skill_tree:
+        element = skill_tree.pop(0)
         if skill[0] == element:
             skill.pop(0)
         else:
@@ -11,9 +13,12 @@ def isAvailable(skill, skills):
 def solution(skill, skill_trees):
     answer = 0
     for skill_tree in skill_trees:
+        # 스킬트리에 있는 것만 skills에 저장
         skills = []
         for s in skill_tree:
             if s in skill: skills.append(s)
+
+        # 가능한 스킬일 경우 count 값 증가
         if isAvailable(list(skill), skills): answer += 1
     return answer
 
